7a03c119df08d0103398ffd1a175199a1a5b3a51
RooOlaRa <roope.raparanta@tuni.fi>
2024-03-15 12:34:37 +0200

    Updated CoinSpawner

--- Assets/Code/CoinSpawner.cs
+++ Assets/Code/CoinSpawner.cs
@@ -11,9 +11,16 @@ namespace Mobiiliesimerkki
         [SerializeField] private GameObject m_CoinPrefab;
         [SerializeField] private float m_SpawnRadius = 5.0f;
         private float m_SpawnTimer = 0.0f;
+        private int m_MaxCoins = 5;
+        private int m_CoinCount = 0;
 
         private void Update()
         {
+            if (m_CoinCount >= m_MaxCoins)
+            {
+                Destroy(gameObject);
+            }
+            
             m_SpawnTimer += Time.deltaTime;
             if (m_SpawnTimer >= m_SpawnInterval)
             {
@@ -27,6 +34,7 @@ namespace Mobiiliesimerkki
             Vector3 spawnPosition = transform.position + Random.insideUnitSphere * m_SpawnRadius;
             spawnPosition.z = -1.0f;
             Instantiate(m_CoinPrefab, spawnPosition, Quaternion.identity);
+            m_CoinCount++;
         }
     }
 }
